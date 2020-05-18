8dbe6ca2b2d4b6b587f0534b7018740cda928f34
Marcelo <marcelo197519@gmail.com>
2020-05-18 02:05:53 -0300

    alterado

--- main.dart
+++ main.dart
@@ -1,9 +1,11 @@
 import'./pessoa.dart';
 main(){
    var  p1=Pessoa(nome:"Marcelo",idade:45);
-    print("Nome: ${p1.nome} idade:${p1.idade ?? "1000 anos"}");
-    p1.status();
-
-	var p2=Pessoa();
-	p2.status();
+	 print("Nome: ${p1.nome} idade:${p1.idade ?? "1000 anos"}");
+	 p1.status();
+	 var p2=Pessoa();
+	 p2.status();
+	 p1.falar();
+	 p2.calar();
+    
 }
--- pessoa.dart
+++ pessoa.dart
@@ -2,6 +2,8 @@ class Pessoa{
 	String nome;
 	int    idade;
 	Pessoa({this.nome,this.idade});
-	
 	void status()=>print("Nome:${nome ?? "Programador" } Idade:${idade ?? "1000 anos" }");
+	
+	void falar()   => print("${nome ?? Pessoa} esta falando !!");
+	void calar()  => print("${nome ?? Pessoa} esta calada !!"); 
 }
